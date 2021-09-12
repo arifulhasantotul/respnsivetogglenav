0a64ac17706f27a800b6e1d34177f97f88cbd25c
arifulhasantotul <77568259+arifulhasantotul@users.noreply.github.com>
2021-09-12 10:29:29 +0600

    search btn

--- js/app.js
+++ js/app.js
@@ -6,4 +6,28 @@ menu.addEventListener('click', () => {
     // toggle use for classlist to add or remove
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
+});const menu = document.getElementById('menu_bars');
+const searchForm = document.getElementById('search_form');
+const searchBtn = document.getElementById('search_icon');
+const closeBtn = document.getElementById('close');
+const navbar = document.querySelector('.navbar');
+
+// navbar toggle done 
+menu.addEventListener('click', () => {
+    // toggle use for classlist to add or remove
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
 });
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+// search modal open 
+searchBtn.onclick = () => {
+    searchForm.classList.toggle('active');
+}
+// search modal close 
+closeBtn.addEventListener('click', () => {
+    searchForm.classList.remove('active');
+})
